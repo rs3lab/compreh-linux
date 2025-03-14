61010bdc80f3c73e6289eab98c2cd1b7ec238e25
Luis Gerhorst <luis.gerhorst@fau.de>
2025-03-14 19:11:32 +0100

    bpf: Remove array_index_nospec() for skb
    Not required as shown in
    https://lore.kernel.org/bpf/20230928110927.115238-1-gerhorst@amazon.de/

--- net/core/filter.c
+++ net/core/filter.c
@@ -239,10 +239,8 @@ BPF_CALL_4(bpf_skb_load_helper_8, const struct sk_buff *, skb, const void *,
 	const int len = sizeof(tmp);
 
 	if (offset >= 0) {
-		if (headlen - offset >= len) {
-			offset = array_index_nospec(offset, headlen-len);
+		if (headlen - offset >= len)
 			return *(u8 *)(data + offset);
-		}
 		if (!skb_copy_bits(skb, offset, &tmp, sizeof(tmp)))
 			return tmp;
 	} else {
@@ -297,10 +295,8 @@ BPF_CALL_4(bpf_skb_load_helper_16, const struct sk_buff *, skb, const void *,
 	const int len = sizeof(tmp);
 
 	if (offset >= 0) {
-		if (headlen - offset >= len) {
-			offset = array_index_nospec(offset, headlen-len);
+		if (headlen - offset >= len)
 			return get_unaligned_be16(data + offset);
-		}
 		if (!skb_copy_bits(skb, offset, &tmp, sizeof(tmp)))
 			return be16_to_cpu(tmp);
 	} else {
@@ -355,10 +351,8 @@ BPF_CALL_4(bpf_skb_load_helper_32, const struct sk_buff *, skb, const void *,
 	const int len = sizeof(tmp);
 
 	if (likely(offset >= 0)) {
-		if (headlen - offset >= len) {
-			offset = array_index_nospec(offset, headlen-len);
+		if (headlen - offset >= len)
 			return get_unaligned_be32(data + offset);
-		}
 		if (!skb_copy_bits(skb, offset, &tmp, sizeof(tmp)))
 			return be32_to_cpu(tmp);
 	} else {
